c978cddc6c2f7b6f7b0d824aebd988fe3d6f2760
Havva Firtina <Havva.Firtina@gmail.com>
2026-05-16 01:02:55 +0200

    Initial commit

---
+++ ZomPart/ZomPart/ZomPartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZomPartApp.swift
+//  ZomPart
+//
+//  Created by Havva Fırtına on 2026-05-16.
+//
+
+import SwiftUI
+
+@main
+struct ZomPartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
